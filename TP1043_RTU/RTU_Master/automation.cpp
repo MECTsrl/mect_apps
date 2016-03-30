85a5bb258e7c67404d6dc60143842e40799e794a
Emiliano Bergamaschini <ebergamaschini@mect.it>
2016-03-30 11:49:16 +0200

    add TP1043_RTU/RTU_Master/ and TP1043_RTU/RTU_Slave/

---
+++ TP1043_RTU/RTU_Master/automation.cpp
@@ -0,0 +1,14 @@
+#include "crosstable.h"
+
+/* put here the initalization */
+void setup(void)
+{
+    
+}
+
+/* put here the operation made every 100ms */
+void loop(void)
+{
+    
+}
+
---
+++ TP1043_RTU/RTU_Slave/languages_en.ts
@@ -0,0 +1,255 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="26"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="62"/>
+        <location filename="page100.ui" line="979"/>
+        <source>65535</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="65"/>
+        <location filename="page100.ui" line="71"/>
+        <source>RTU_HEARTBEAT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="68"/>
+        <location filename="page100.ui" line="118"/>
+        <location filename="page100.ui" line="578"/>
+        <location filename="page100.ui" line="736"/>
+        <location filename="page100.ui" line="824"/>
+        <location filename="page100.ui" line="923"/>
+        <location filename="page100.ui" line="985"/>
+        <location filename="page100.ui" line="1115"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="77"/>
+        <location filename="page100.ui" line="80"/>
+        <location filename="page100.ui" line="127"/>
+        <location filename="page100.ui" line="130"/>
+        <location filename="page100.ui" line="587"/>
+        <location filename="page100.ui" line="590"/>
+        <location filename="page100.ui" line="745"/>
+        <location filename="page100.ui" line="748"/>
+        <location filename="page100.ui" line="833"/>
+        <location filename="page100.ui" line="836"/>
+        <location filename="page100.ui" line="932"/>
+        <location filename="page100.ui" line="935"/>
+        <location filename="page100.ui" line="997"/>
+        <location filename="page100.ui" line="1000"/>
+        <location filename="page100.ui" line="1124"/>
+        <location filename="page100.ui" line="1127"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="115"/>
+        <location filename="page100.ui" line="121"/>
+        <location filename="page100.ui" line="1203"/>
+        <location filename="page100.ui" line="1209"/>
+        <source>RTU0_STATUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="173"/>
+        <location filename="page100.ui" line="332"/>
+        <location filename="page100.ui" line="453"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="176"/>
+        <source>menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="205"/>
+        <location filename="page100.ui" line="208"/>
+        <source>TP1043_485</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="252"/>
+        <source>READS / WRITES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="274"/>
+        <location filename="page100.ui" line="280"/>
+        <source>PLC_EngineStatus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="277"/>
+        <location filename="page100.ui" line="631"/>
+        <location filename="page100.ui" line="1206"/>
+        <source>ATCMcombobox widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="308"/>
+        <source>1;INITIALIZED;2;RUNNING;3;ERROR;4;EXITING;0;IDLE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="329"/>
+        <location filename="page100.ui" line="341"/>
+        <source>PLC_ResetValues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="364"/>
+        <location filename="page100.ui" line="367"/>
+        <source>Reset Values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="395"/>
+        <source>STATUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="411"/>
+        <location filename="page100.ui" line="417"/>
+        <source>RTU_RTU1_RD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="414"/>
+        <source>ATCMled widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="450"/>
+        <location filename="page100.ui" line="462"/>
+        <source>RTU_RTU1_WR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="521"/>
+        <source>RTU_SRV
+RTU 0 NODE 2</source>
+        <oldsource>RTU1
+RTU0 / NODE 2</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="543"/>
+        <source>ATCM QTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="546"/>
+        <source>ATCMtime widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="575"/>
+        <location filename="page100.ui" line="581"/>
+        <source>RTU0_WRITES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="628"/>
+        <location filename="page100.ui" line="634"/>
+        <source>NODE_01_STATUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="662"/>
+        <source>1;NODE_OK;2;TIMEOUT;3;BLACKLIST;0;NO_NODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="698"/>
+        <source>ATCM QDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="701"/>
+        <source>ATCMdate widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="733"/>
+        <location filename="page100.ui" line="739"/>
+        <source>RTU0_READS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="802"/>
+        <source>Heartbeat / WR / RD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="821"/>
+        <location filename="page100.ui" line="827"/>
+        <source>RTU0_TIMEOUTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="879"/>
+        <source>PLC Engine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="917"/>
+        <source>86400.00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="920"/>
+        <location filename="page100.ui" line="926"/>
+        <source>PLC_time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="982"/>
+        <location filename="page100.ui" line="988"/>
+        <source>PLC_Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="991"/>
+        <source>Version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1054"/>
+        <source>NODE STATUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1079"/>
+        <source>Modbus RTU
+Slave / Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1112"/>
+        <location filename="page100.ui" line="1118"/>
+        <source>RTU0_COMM_ERRORS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1168"/>
+        <source>TIMEOUTS / COMM_ERR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1237"/>
+        <source>1;NOT_CONNECTED;2;CONNECTED;3;CONNECTED_WITH_ERRORS;4;DEVICE_BLACKLIST;5;NO_HOPE;0;ZERO</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
