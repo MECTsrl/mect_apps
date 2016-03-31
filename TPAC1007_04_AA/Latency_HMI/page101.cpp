61fd39cd83e04e5903dac62828306ae594b45fd5
Francesco Mandracci <fmandracci@mect.it>
2016-03-31 16:06:13 +0200

    Missing commits.

---
+++ TPAC1007_04_AA/Latency_HMI/page101.cpp
@@ -0,0 +1,99 @@
+/**
+ * @file
+ * @author Emiliano Bergamaschini <ebergamaschini@mect.it>
+ *
+ * @section LICENSE
+ * Copyright Mect s.r.l. 2013
+ *
+ * @brief Generic page
+ */
+#include "app_logprint.h"
+#include "atcmplugin.h"
+#include "main.h"
+#include "page101.h"
+#include "ui_page101.h"
+
+/**
+ * @brief this macro is used to set the PAGE101 style.
+ * the syntax is html stylesheet-like
+ */
+#define SET_PAGE101_STYLE() { \
+    QString mystyle; \
+    mystyle.append(this->styleSheet()); \
+    /* add there the page stilesheet customization */ \
+    mystyle.append(""); \
+    this->setStyleSheet(mystyle); \
+    }
+
+/**
+ * @brief This is the constructor. The operation written here, are executed only one time: at the instantiation of the page.
+ */
+page101::page101(QWidget *parent) :
+    page(parent),
+    ui(new Ui::page101)
+{
+    ui->setupUi(this);
+    /* set here the protection level (pwd_admin_e, pwd_super_user_e, pwd_user_e, pwd_operator_e), default is pwd_operator_e
+     * protection_level = pwd_operator_e;
+     */
+    
+    /* set up the page style */
+    //SET_PAGE_STYLE();
+    /* set the style described into the macro SET_PAGE101_STYLE */
+    SET_PAGE101_STYLE();
+    translateFontSize(this);
+}
+
+/**
+ * @brief This is the reload member. The operation written here, are executed every time this page is shown.
+ */
+void page101::reload()
+{
+    /*
+       H variables initalizations:
+         variableList.clear();
+         variableList << "VARIABLE1" << "VARIABLE2" << "VARIABLE3"
+       alarm banner initialization in QLineEdit:
+         rotateShowError(ui->myLineEdit, ERROR_LABEL_PERIOD_MS);
+     */
+}
+
+/**
+ * @brief This is the updateData member. The operation written here, are executed every REFRESH_MS milliseconds.
+ */
+void page101::updateData()
+{
+    if (this->isVisible() == false)
+    {
+        return;
+    }
+    /* call the parent updateData member */
+    page::updateData();
+    
+    /* To read the cross table variable UINT TEST1:
+     *    uint_16 tmp = TEST1;
+     */
+    /* To write 5 into the the cross table variable UINT TEST1:
+     *    doWrite_TEST1(5);
+     */	
+}
+
+/**
+ * @brief This is the event slot to detect new language translation.
+ */
+void page101::changeEvent(QEvent * event)
+{
+    if (event->type() == QEvent::LanguageChange)
+    {
+        ui->retranslateUi(this);
+    }
+}
+
+/**
+ * @brief This is the distructor member. The operation written here, are executed only one time when the page will be deleted.
+ */
+page101::~page101()
+{
+    delete ui;
+}
+
