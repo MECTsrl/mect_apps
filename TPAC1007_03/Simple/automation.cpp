4258c49550b63fdbb11572c4591eaa086e20d6f6
Francesco Mandracci <fmandracci@mect.it>
2016-05-20 10:11:29 +0200

    TPAC1007_03 Simple: deleted unused dig.vars. and forced An.I/O config in setup().

--- TPAC1007_03/Simple/automation.cpp
+++ TPAC1007_03/Simple/automation.cpp
@@ -3,7 +3,9 @@
 /* put here the initalization */
 void setup(void)
 {
-    
+    doWrite_PLC_AnInConf_1(2); // 2 = [V] 0..10
+    doWrite_PLC_AnInConf_2(2); // 2 = [V] 0..10
+    doWrite_PLC_AnOutConf_1(2); // 2 = [V] 0..10
 }
 
 /* put here the operation made every 100ms */
