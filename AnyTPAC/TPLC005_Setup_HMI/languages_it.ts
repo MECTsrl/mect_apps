ee4e9a15900faa1b4fe24b1d2c3b597903094f68
Francesco Mandracci <fmandracci@mect.it>
2016-06-28 19:25:31 +0200

    AnyTPAC TPLC005_Setup_HMI.

---
+++ AnyTPAC/TPLC005_Setup_HMI/languages_it.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="26"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="636"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="639"/>
+        <location filename="page100.ui" line="654"/>
+        <source>menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="651"/>
+        <source>AnyTPAC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="309"/>
+        <location filename="page100.ui" line="315"/>
+        <source>Actual_Baudrate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="46"/>
+        <location filename="page100.ui" line="95"/>
+        <location filename="page100.ui" line="150"/>
+        <location filename="page100.ui" line="205"/>
+        <location filename="page100.ui" line="257"/>
+        <location filename="page100.ui" line="312"/>
+        <location filename="page100.ui" line="349"/>
+        <location filename="page100.ui" line="383"/>
+        <location filename="page100.ui" line="417"/>
+        <location filename="page100.ui" line="451"/>
+        <location filename="page100.ui" line="485"/>
+        <location filename="page100.ui" line="519"/>
+        <location filename="page100.ui" line="553"/>
+        <location filename="page100.ui" line="587"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="318"/>
+        <source>Actual_Baudrate </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="55"/>
+        <location filename="page100.ui" line="58"/>
+        <location filename="page100.ui" line="107"/>
+        <location filename="page100.ui" line="110"/>
+        <location filename="page100.ui" line="165"/>
+        <location filename="page100.ui" line="168"/>
+        <location filename="page100.ui" line="217"/>
+        <location filename="page100.ui" line="220"/>
+        <location filename="page100.ui" line="269"/>
+        <location filename="page100.ui" line="272"/>
+        <location filename="page100.ui" line="324"/>
+        <location filename="page100.ui" line="327"/>
+        <location filename="page100.ui" line="358"/>
+        <location filename="page100.ui" line="361"/>
+        <location filename="page100.ui" line="392"/>
+        <location filename="page100.ui" line="395"/>
+        <location filename="page100.ui" line="426"/>
+        <location filename="page100.ui" line="429"/>
+        <location filename="page100.ui" line="460"/>
+        <location filename="page100.ui" line="463"/>
+        <location filename="page100.ui" line="494"/>
+        <location filename="page100.ui" line="497"/>
+        <location filename="page100.ui" line="528"/>
+        <location filename="page100.ui" line="531"/>
+        <location filename="page100.ui" line="562"/>
+        <location filename="page100.ui" line="565"/>
+        <location filename="page100.ui" line="596"/>
+        <location filename="page100.ui" line="599"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="202"/>
+        <location filename="page100.ui" line="208"/>
+        <source>Actual_Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="211"/>
+        <source>Actual_Address </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="43"/>
+        <location filename="page100.ui" line="49"/>
+        <source>RTU3_BAUDRATE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="147"/>
+        <location filename="page100.ui" line="153"/>
+        <source>LifeTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="156"/>
+        <source>LifeTime </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="159"/>
+        <source>s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="254"/>
+        <location filename="page100.ui" line="260"/>
+        <source>FutureBaudrate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="263"/>
+        <source>FutureBaudrate </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="346"/>
+        <location filename="page100.ui" line="352"/>
+        <source>RTU3_WRITE_QUEUE</source>
+        <oldsource>RTU0_WRITE_QUEUE</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="380"/>
+        <location filename="page100.ui" line="386"/>
+        <source>RTU3_READ_QUEUE</source>
+        <oldsource>RTU0_READ_QUEUE</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="414"/>
+        <location filename="page100.ui" line="420"/>
+        <source>RTU3_READS</source>
+        <oldsource>RTU0_READS</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="448"/>
+        <location filename="page100.ui" line="454"/>
+        <source>RTU3_WRITES</source>
+        <oldsource>RTU0_WRITES</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="482"/>
+        <location filename="page100.ui" line="488"/>
+        <source>RTU3_COMM_ERRORS</source>
+        <oldsource>RTU0_COMM_ERRORS</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="516"/>
+        <location filename="page100.ui" line="522"/>
+        <source>RTU3_TIMEOUTS</source>
+        <oldsource>RTU0_TIMEOUTS</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="676"/>
+        <source>TPLC005 Setup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="550"/>
+        <location filename="page100.ui" line="556"/>
+        <source>RTU3_STATUS</source>
+        <oldsource>RTU0_STATUS</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="584"/>
+        <location filename="page100.ui" line="590"/>
+        <source>RTU3_TYPE_PORT</source>
+        <oldsource>RTU0_TYPE_PORT</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="92"/>
+        <location filename="page100.ui" line="98"/>
+        <source>FutureAddress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="101"/>
+        <source>FutureAddress </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
