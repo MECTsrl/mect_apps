6e9c3a0e43fb45995f00b79340883beeeda8f3a6
fmandracci <fmandracci@mect.it>
2016-07-23 09:19:30 +0200

    Octopus USB fix

--- TPAC1007_04_AA/Octopus_Prey_USB/sysupdate_usb_factory.sh
+++ TPAC1007_04_AA/Octopus_Prey_USB/sysupdate_usb_factory.sh
@@ -7,7 +7,6 @@ echo 100 > /sys/devices/platform/mxs-bl.0/backlight/mxs-bl/brightness
 usbdev=/dev/sda
 test -e /sys/block/sda/sda1 && usbdev=/dev/sda1
 mntpoint=/mnt/floppy
-test -d $mntpoint || mkdir -p $mntpoint
 mkdir -p $mntpoint
 mount -o sync $usbdev $mntpoint
 
@@ -31,7 +30,7 @@ ln -s $mntdir/root root 2>/dev/null
 cp /local/flash/root/fcrts /root/
 
 rm -f etc
-cp -r /local/flash/etc $mntdir/etc
+test -d $mntdir/etc || cp -r /local/flash/etc $mntdir/etc
 ln -s $mntdir/etc etc 2>/dev/null
 /etc/rc.d/init.d/network restart
 
