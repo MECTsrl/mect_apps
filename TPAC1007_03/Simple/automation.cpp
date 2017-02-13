84a8a36f893110a506fbad396d932341abf4fd21
fmandracci <fmandracci@mect.it>
2017-02-13 12:25:39 +0100

    TPAC1007_03/Simple: rewrote initial values in setup()

--- TPAC1007_03/Simple/automation.cpp
+++ TPAC1007_03/Simple/automation.cpp
@@ -3,6 +3,14 @@
 /* put here the initalization */
 void setup(void)
 {
+    doWrite_PLC_AnInConf_1(2); // 2 = [V] 0..10
+    doWrite_PLC_AnInConf_2(2); // 2 = [V] 0..10
+
+    doWrite_PLC_EncEnable(2); // 2 = Counter
+
+    doWrite_PLC_AnOutConf_1(2); // 2 = [V] 0..10
+    doWrite_PLC_AnOut_1(333); // 3,33 V
+
     doWrite_PLC_DigDir_1(1); // 1 = output
     doWrite_PLC_DigDir_2(1); // 1 = output
     doWrite_PLC_DigDir_3(1); // 1 = output
@@ -13,9 +21,6 @@ void setup(void)
     doWrite_PLC_DigDir_7(0); // 0 = input
     doWrite_PLC_DigDir_8(0); // 0 = input
 
-    doWrite_PLC_AnInConf_1(2); // 2 = [V] 0..10
-    doWrite_PLC_AnInConf_2(2); // 2 = [V] 0..10
-    doWrite_PLC_AnOutConf_1(2); // 2 = [V] 0..10
 }
 
 /* put here the operation made every 100ms */
