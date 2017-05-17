8aeece31fe7c006568a477f5d977fb8d2dde6e29
Francesco Mandracci <fmandracci@mect.it>
2017-05-17 21:31:56 +0200

    TPLC100_01_AA/Simple v3.0

---
+++ TPLC100_01_AA/Simple/languages_en.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="26"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="43"/>
+        <source>page: menu  status: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="46"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="61"/>
+        <location filename="page100.ui" line="64"/>
+        <source>TPLC100_01_AA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="92"/>
+        <source>ATCM QDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="95"/>
+        <source>ATCMdate widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="123"/>
+        <source>ATCM QTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="126"/>
+        <source>ATCMtime widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="151"/>
+        <location filename="page100.ui" line="157"/>
+        <source>PLC_Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="154"/>
+        <location filename="page100.ui" line="200"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="197"/>
+        <location filename="page100.ui" line="203"/>
+        <source>PLC_time</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
