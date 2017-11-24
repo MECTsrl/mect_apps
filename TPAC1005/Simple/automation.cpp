df32a4754ff35173db322b1d60b8eb4c4b9994b1
Francesco Mandracci <fmandracci@mect.it>
2017-11-24 16:57:01 +0100

    update TPAC1005/Simple (first part)

--- TPAC1005/Simple/automation.cpp
+++ TPAC1005/Simple/automation.cpp
@@ -1,66 +1,75 @@
 #include "crosstable.h"
 #include "automation.h"
+
 #include <linux/input.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
 // Function Keys
-#define F01 0x41
-#define F02 0x42
-#define F03 0x44
-#define F04 0x3C
-#define F05 0x3E
-#define F06 0x3F
-#define F07 0x3B
-#define F08 0x40
-#define F09 0x3D
-#define F10 0x43
 
+#define F01 0x41 // T7  PLC_FastIO_14
+#define F02 0x42 // T8  PLC_FastIO_15
+#define F03 0x44 // T10 PLC_FastIO_17
+#define F04 0x3C // T2  PLC_FastIO_1
+#define F05 0x3E // T4  PLC_FastIO_11
+
+#define F06 0x3F // T5  PLC_FastIO_12
+#define F07 0x3B // T1  PLC_FastIO_9
+#define F08 0x40 // T6  PLC_FastIO_13
+#define F09 0x3D // T3  PLC_FastIO_10
+#define F10 0x43 // T9  PLC_FastIO_16
 
 static int fd;
 
 /* put here the initalization */
 void setup(void)
 {
+    doWrite_PLC_FastIO_Ena(0x0003FF01);
+    doWrite_PLC_FastIO_Dir(0x00020000); // PLC_FastIO_18 --> GPIO(2,9) PIN275(SSP0_DETECT) [R77]
+
     fd = open("/dev/input/event0", O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "error opening event0\n");
+    }
 }
 
 /* put here the operation made every 100ms */
 void loop(void)
 {
-    struct input_event iev;
-    int value;
-
-    int retVal = 0;
     if (fd >= 0)  {
-        retVal = read(fd, &iev, sizeof(struct input_event));
+        struct input_event iev;
+        int retVal = read(fd, &iev, sizeof(struct input_event));
+
         if (retVal == sizeof(iev))  {
             if (iev.type == EV_KEY)  {
                 if (iev.value == 0)  {
-                    fprintf(stderr, "Key Pressed: %d\n", iev.code);
-                    value = 1;
+                    fprintf(stderr, "key %d pressed ", iev.code);
                 }
                 else if (iev.value == 1)  {
-                    fprintf(stderr, "Key Released: %d\n", iev.code);
-                    value = 0;
+                    fprintf(stderr, "key %d released ", iev.code);
                 }
                 else {
+                    fprintf(stderr, "key %d unknown value %d\n", iev.code, iev.value);
                     return;
                 }
                 switch (iev.code)  {
-                case F01: doWrite_PLC_Key_01(value); break;
-                case F02: doWrite_PLC_Key_02(value); break;
-                case F03: doWrite_PLC_Key_03(value); break;
-                case F04: doWrite_PLC_Key_04(value); break;
-                case F05: doWrite_PLC_Key_05(value); break;
-                case F06: doWrite_PLC_Key_06(value); break;
-                case F07: doWrite_PLC_Key_07(value); break;
-                case F08: doWrite_PLC_Key_08(value); break;
-                case F09: doWrite_PLC_Key_09(value); break;
-                case F10: doWrite_PLC_Key_10(value); break;
-                default:;
+                case F01: fputs("TASTO 1\n", stderr); break;
+                case F02: fputs("TASTO 2\n", stderr); break;
+                case F03: fputs("TASTO 3\n", stderr); break;
+                case F04: fputs("TASTO 4\n", stderr); break;
+                case F05: fputs("TASTO 5\n", stderr); break;
+                case F06: fputs("TASTO 6\n", stderr); break;
+                case F07: fputs("TASTO 7\n", stderr); break;
+                case F08: fputs("TASTO 8\n", stderr); break;
+                case F09: fputs("TASTO 9\n", stderr); break;
+                case F10: fputs("TASTO 10\n", stderr); break;
+                default: fputs("TASTO ??\n", stderr);
                 }
+            } else if (iev.type == EV_SYN) {
+                return;
+            } else {
+                fprintf(stderr, "unknown event type:%d\n", iev.type);
             }
         }
     }
