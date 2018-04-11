359bae8be4f8bca8e5b7fadc72021d8046497949
Francesco Mandracci <fmandracci@mect.it>
2018-04-11 17:57:48 +0200

    AnyTPAC/DemoPanelPC

---
+++ AnyTPAC/DemoPanelPC/automation.cpp
@@ -0,0 +1,84 @@
+#include "crosstable.h"
+#include "automation.h"
+
+void update_all_json();
+void do_write_query();
+
+/* put here the initalization */
+void setup(void)
+{
+    doWrite_theHEARTBEAT(0.0);
+}
+
+/* put here the operation made every 100ms */
+void loop(void)
+{
+    do_write_query();
+    update_all_json();
+}
+
+void update_all_json()
+{
+    static int counter = 0;
+
+    // update each 0.5 s
+    if (counter % 5 == 0) {
+
+        FILE *file_json = fopen("/tmp/update_all.json", "w+");
+        if (file_json) {
+            fprintf(file_json,
+                    "Content-Type: application/json; charset=UTF-8\n"
+                    "\n"
+                    "{\n");
+
+            fprintf(file_json, "\"theHEARTBEAT\" :\"%0.2f\",\n", theHEARTBEAT);
+            fprintf(file_json, "\"theINPUT\"     :\"%d\",\n", theINPUT);
+            fprintf(file_json, "\"theOUTPUT\"    :\"%d\",\n", theOUTPUT);
+
+            fprintf(file_json, "\"PLC_Year\"     :\"%u\",\n", PLC_Year);
+            fprintf(file_json, "\"PLC_Month\"    :\"%u\",\n", PLC_Month);
+            fprintf(file_json, "\"PLC_Day\"      :\"%u\",\n", PLC_Day);
+
+            fprintf(file_json, "\"PLC_Hours\"    :\"%u\",\n", PLC_Hours);
+            fprintf(file_json, "\"PLC_Minutes\"  :\"%u\",\n", PLC_Minutes);
+            fprintf(file_json, "\"PLC_Seconds\"  :\"%u\" \n", PLC_Seconds);
+
+            fprintf(file_json,"}\n");
+            fclose(file_json);
+        } else {
+            fprintf(stderr, "update_all_json(): cannot write json\n");
+        }
+
+        doWrite_theHEARTBEAT(theHEARTBEAT + 0.5);
+    }
+    ++counter;
+}
+
+void do_write_query()
+{
+    FILE *file_query = fopen("/tmp/do_write.query", "r");
+
+    if (!file_query) {
+        return;
+    }
+    fprintf(stderr, "new file\n");
+    while (! feof(file_query)) {
+        char name[42];
+        float fvalue;
+
+        if (fscanf(file_query, "%41[^=]=%f ", name, &fvalue) != 2) {
+            fprintf(stderr, "bad fscanf\n");
+            break;
+        }
+        if (strcmp(name, "theINPUT") == 0) {
+            int ivalue = (int)fvalue;
+
+            fprintf(stderr, "doWrite_theINPUT(%d)\n", ivalue);
+            doWrite_theINPUT(ivalue);
+        }
+    }
+    fclose(file_query);
+    fprintf(stderr, "unlink\n");
+    unlink("/tmp/do_write.query");
+}
+
---
+++ AnyTPAC/DemoPanelPC/languages_it.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="26"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="53"/>
+        <source>YYYY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="56"/>
+        <location filename="page100.ui" line="62"/>
+        <source>PLC_Year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="59"/>
+        <location filename="page100.ui" line="132"/>
+        <location filename="page100.ui" line="205"/>
+        <location filename="page100.ui" line="278"/>
+        <location filename="page100.ui" line="351"/>
+        <location filename="page100.ui" line="424"/>
+        <location filename="page100.ui" line="658"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="126"/>
+        <source>MM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="129"/>
+        <location filename="page100.ui" line="135"/>
+        <source>PLC_Month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="199"/>
+        <source>DD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="202"/>
+        <location filename="page100.ui" line="208"/>
+        <source>PLC_Day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="272"/>
+        <source>hh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="275"/>
+        <location filename="page100.ui" line="281"/>
+        <source>PLC_Hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="345"/>
+        <source>mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="348"/>
+        <location filename="page100.ui" line="354"/>
+        <source>PLC_Minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="418"/>
+        <source>ss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="421"/>
+        <location filename="page100.ui" line="427"/>
+        <source>PLC_Seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="488"/>
+        <source>ATCM QDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="491"/>
+        <source>ATCMdate widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="540"/>
+        <source>ATCM QTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="543"/>
+        <source>ATCMtime widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="592"/>
+        <location filename="page100.ui" line="773"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="621"/>
+        <location filename="page100.ui" line="624"/>
+        <source>menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="652"/>
+        <source>4000111000</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="655"/>
+        <location filename="page100.ui" line="661"/>
+        <source>theHEARTBEAT</source>
+        <oldsource>HEARTBEAT</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="716"/>
+        <location filename="page100.ui" line="728"/>
+        <source>theINPUT</source>
+        <oldsource>INPUT</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="719"/>
+        <source>ATCMled widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="770"/>
+        <location filename="page100.ui" line="782"/>
+        <source>theOUTPUT</source>
+        <oldsource>OUTPUT</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="819"/>
+        <source>off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="822"/>
+        <source>ON</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
