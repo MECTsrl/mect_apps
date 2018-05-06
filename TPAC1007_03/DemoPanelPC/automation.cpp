e89996dcae46455929506a4ee7f1e19e66e5c67b
Francesco Mandracci <fmandracci@mect.it>
2018-05-06 20:17:56 +0200

    work in progress

---
+++ TPAC1007_03/DemoPanelPC/automation.cpp
@@ -0,0 +1,71 @@
+#include "crosstable.h"
+#include "automation.h"
+
+void update_all_json();
+void do_write_query();
+
+/* put here the initalization */
+void setup(void)
+{
+}
+
+/* put here the operation made every 100ms */
+void loop(void)
+{
+    do_write_query();
+    update_all_json();
+}
+
+void update_all_json()
+{
+    static int counter = 0;
+
+    // update each 0.5 s
+    if (counter % 5 == 0) {
+
+        FILE *file_json = fopen("/tmp/update_all.json", "w");
+        if (file_json) {
+            fprintf(file_json,
+                    "Content-Type: application/json; charset=UTF-8\n"
+                    "\n"
+                    "{\n");
+
+            fprintf(file_json, "\"theHEARTBEAT\" :\"%u\",\n", theHEARTBEAT);
+            fprintf(file_json, "\"theLED\"       :\"%d\",\n", theLED);
+            fprintf(file_json, "\"theBUTTON\"    :\"%d\",\n", theBUTTON);
+
+            fprintf(file_json,"}\n");
+            fclose(file_json);
+        } else {
+            fprintf(stderr, "update_all_json(): cannot write json\n");
+        }
+    }
+    ++counter;
+}
+
+void do_write_query()
+{
+    FILE *file_query = fopen("/tmp/do_write.query", "r");
+
+    if (!file_query) {
+        return;
+    }
+    while (! feof(file_query)) {
+        char name[42];
+        int ivalue;
+
+        if (fscanf(file_query, "%41[^=]=%d ", name, &ivalue) != 2) {
+            fprintf(stderr, "bad fscanf\n");
+            break;
+        }
+        if (strcmp(name, "theLED") == 0) {
+
+            fprintf(stderr, "doWrite_theLED(%d)\n", ivalue);
+            doWrite_theLED(ivalue);
+
+            doWrite_HTML5_writes(HTML5_writes + 1);
+        }
+    }
+    fclose(file_query);
+    unlink("/tmp/do_write.query");
+}
---
+++ TPAC1007_03/DemoPanelPC/languages_it.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="29"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="101"/>
+        <location filename="page100.ui" line="206"/>
+        <location filename="page100.ui" line="519"/>
+        <location filename="page100.ui" line="592"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="69"/>
+        <source>VNC
+Android</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="95"/>
+        <location filename="page100.ui" line="200"/>
+        <location filename="page100.ui" line="586"/>
+        <source>123456</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="98"/>
+        <location filename="page100.ui" line="104"/>
+        <source>HTML5_writes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="174"/>
+        <source>Modbus/TCP
+Android</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="430"/>
+        <location filename="page100.ui" line="442"/>
+        <source>theBUTTON</source>
+        <oldsource>theLED</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="203"/>
+        <location filename="page100.ui" line="209"/>
+        <source>TCPS_READS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="279"/>
+        <source>HTML5
+Android</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="321"/>
+        <source>Eng./TCP
+Android</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="350"/>
+        <location filename="page100.ui" line="433"/>
+        <location filename="page100.ui" line="686"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="393"/>
+        <location filename="page100.ui" line="396"/>
+        <source>menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="479"/>
+        <source>red off</source>
+        <oldsource>off</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="482"/>
+        <source>red ON</source>
+        <oldsource>ON</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="513"/>
+        <source>4000111000</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="516"/>
+        <location filename="page100.ui" line="522"/>
+        <source>theHEARTBEAT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="589"/>
+        <location filename="page100.ui" line="595"/>
+        <source>TCPS_WRITES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="683"/>
+        <location filename="page100.ui" line="695"/>
+        <source>theLED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="732"/>
+        <source>blu off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="735"/>
+        <source>blu ON</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
