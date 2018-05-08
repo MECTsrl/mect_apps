b422a945b5b6a62595e5ed6cf243d37544c955fe
Francesco Mandracci <fmandracci@mect.it>
2018-05-08 17:43:25 +0200

    work in progess

--- AnyTPAC/DemoPanelPC/demo-react/src/page100.js
+++ AnyTPAC/DemoPanelPC/demo-react/src/page100.js
@@ -17,9 +17,7 @@ class Page100 extends Component {
         theHEARTBEAT : 0,
         theLED : 0,
         theBUTTON : 0,
-        readStatus : "?",
-        writeStatus : "?"
-
+        msg : ""
     };
     this.axios = require('axios');
   }
@@ -29,42 +27,41 @@ class Page100 extends Component {
   }
 
   updateData() {
-    let thisPage = this;
+        let thisPage = this;
 
-    //    root@mect /local/flash/root$ cat /tmp/update_all.json
-    //    Content-Type: application/json; charset=UTF-8
-    //
-    //    {
-    //    "theHEARTBEAT" :"510",
-    //    "theLED"       :"0",
-    //    "theBUTTON"    :"1",
-    //    "PLC_Year"     :"2018",
-    //    "PLC_Month"    :"4",
-    //    "PLC_Day"      :"11",
-    //    "PLC_Hours"    :"13",
-    //    "PLC_Minutes"  :"49",
-    //    "PLC_Seconds"  :"34",
-    //    }
+        //    root@mect /local/flash/root$ cat /tmp/update_all.json
+        //    Content-Type: application/json; charset=UTF-8
+        //
+        //    {
+        //    "theHEARTBEAT" :510,
+        //    "theLED"       :0,
+        //    "theBUTTON"    :1
+        //    }
 
+        this.axios.get("/webmi/update_all.cgi", { }, { timeout: 400 })
+            .then(function (response) {
+                let tmpHEARTBEAT = 0;
+                let tmpLED = 0;
+                let tmpBUTTON = 0;
 
+                tmpHEARTBEAT = response.data.theHEARTBEAT;
+                tmpLED = response.data.theLED;
+                tmpBUTTON = response.data.theBUTTON;
 
-    this.axios.get("/webmi/update_all.cgi", { }, { timeout: 400 })
-        .then(function (response) {
-
-            let tmpHEARTBEAT = response.data.theHEARTBEAT; // parseFloat(response.data.theHEARTBEAT).toFixed(2);
-            let tmpINPUT = response.data.theLED; // parseInt(response.data.theLED, 10);
-            let tmpOUTPUT = response.data.theBUTTON; // parseInt(response.data.theBUTTON, 10);
-
-            thisPage.setState({
-		theHEARTBEAT : tmpHEARTBEAT ,
-		theLED : tmpINPUT , 
-		theBUTTON : tmpOUTPUT ,
-		readStatus : "OK " + response.data
-	    });
-	})
-        .catch(function (error) {
-            thisPage.setState({ readStatus: "ERROR " + error.message });
-        });
+                thisPage.setState({
+                    theHEARTBEAT : tmpHEARTBEAT,
+                    theLED : tmpLED,
+                    theBUTTON : tmpBUTTON
+                });
+            })
+            .catch(function (error) {
+                thisPage.setState({
+                    theHEARTBEAT: -1,
+                    theLED: 0,
+                    theBUTTON: 0,
+                    msg: "update_all OK "
+                });
+            });
   }
 
     componentWillUnmount() {
@@ -76,21 +73,19 @@ class Page100 extends Component {
 
         this.axios.get("/webmi/do_write.cgi", params, { timeout: 400 })
         .then(function (response) {
-            thisPage.setState({
-                writeStatus : "OK " + response.data
-            });
+            thisPage.setState({ msg: "do_write OK " + response.data });
         })
         .catch(function (error) {
-            thisPage.setState({ writeStatus: "ERROR " + error.message });
+            thisPage.setState({ msg: "do_write ERROR " + error.message });
         });
     }
 
     on_INPUT_clicked() {
-	if (this.state.theLED == 0) {
-            this.doWrite({ params: {theLED: 1} });
-	} else {
-            this.doWrite({ params: {theLED: 0} });
-	}
+        if (this.state.theLED) {
+                this.doWrite({ params: {theLED: 0} });
+        } else {
+                this.doWrite({ params: {theLED: 1} });
+        }
     }
 
     render() {
@@ -102,17 +97,16 @@ class Page100 extends Component {
         //            <ol>{renderValues}</ol>
 
         return <div>
-
-	    <p className="page100-style"></p>
-	    <p></p>
-            <p>theHEARTBEAT {this.state.theHEARTBEAT}</p>
-	    <p>theLED <button className={(this.state.theLED == 0)?"button-0":"button-1"} onClick={this.on_INPUT_clicked}>{(this.state.theLED == 0)?"off":"ON"}</button> &lt;-- touch here</p>
-	    <p>theBUTTON <button className={(this.state.theBUTTON == 0)?"button-0":"button-1"}>{(this.state.theBUTTON == 0)?"off":"ON"}</button></p>
-
-            <p className="p-title">demo-react {version} page100</p>
-            <p className="p-msg">readStatus={this.state.readStatus}<br/>writeStatus={this.state.writeStatus}</p>
-
-            </div>;
+        <p><video controls preload="auto" autoplay="autoplay" loop width="100%" muted>
+            <source src="/webmi/video.mp4" type="video/mp4"/>
+            Your browser does not support the video tag.
+        </video></p>
+        <p className="p-barra">
+         &nbsp; <button className="heartbeat">{this.state.theHEARTBEAT}</button>
+         &nbsp; <button className={(this.state.theBUTTON)?"button-1":"button-0"}>{(this.state.theBUTTON)?"red ON":"red off"}</button>
+         &nbsp; <button className={(this.state.theLED)?"led-1":"led-0"} onClick={this.on_INPUT_clicked}> {(this.state.theLED)?"blu ON":"blu off"} </button>
+        </p>
+        </div>;
     }
 }
 
