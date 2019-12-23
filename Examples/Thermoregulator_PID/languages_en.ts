1020cb203d2b4e9282b0ca2efcbf3bc9e7e0eee8
ivan <idemaria@mect.it>
2019-12-23 12:52:27 +0100

    Add Thermoregulaor example

---
+++ Examples/Thermoregulator_PID/languages_en.ts
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="26"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="68"/>
+        <location filename="page100.ui" line="77"/>
+        <source>Start_sw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="71"/>
+        <location filename="page100.ui" line="245"/>
+        <location filename="page100.ui" line="314"/>
+        <location filename="page100.ui" line="730"/>
+        <location filename="page100.ui" line="939"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="164"/>
+        <source>ATCMtime widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="203"/>
+        <source>ATCMdate widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="395"/>
+        <location filename="page100.ui" line="401"/>
+        <source>PLC_AnIn_1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="398"/>
+        <location filename="page100.ui" line="866"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="465"/>
+        <source>Temperature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="page100.ui" line="490"/>
+        <location filename="page100.ui" line="810"/>
+        <source>°C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="506"/>
+        <location filename="page100.ui" line="518"/>
+        <source>PLC_DigOut_1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="509"/>
+        <source>ATCMled widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="557"/>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="582"/>
+        <source>Out 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="607"/>
+        <location filename="page100.ui" line="613"/>
+        <source>Automatic_Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="610"/>
+        <source>ATCMcombobox widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="658"/>
+        <source>0;Manual;1;Automatic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="702"/>
+        <source>ONLY FOR MANUAL MODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="727"/>
+        <location filename="page100.ui" line="739"/>
+        <source>ManualCommandOut_1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="776"/>
+        <location filename="page100.ui" line="779"/>
+        <source>DIG OUT 1 COMMAND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="835"/>
+        <source>Setpoint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="863"/>
+        <location filename="page100.ui" line="869"/>
+        <source>Set_Temperature</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>page200</name>
+    <message>
+        <location filename="page200.ui" line="53"/>
+        <location filename="page200.ui" line="59"/>
+        <source>Set_Temperature_SelfTun</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="56"/>
+        <location filename="page200.ui" line="216"/>
+        <location filename="page200.ui" line="314"/>
+        <location filename="page200.ui" line="387"/>
+        <location filename="page200.ui" line="460"/>
+        <location filename="page200.ui" line="599"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="page200.ui" line="65"/>
+        <location filename="page200.ui" line="225"/>
+        <source> °C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="122"/>
+        <source>SET POINT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="144"/>
+        <location filename="page200.ui" line="156"/>
+        <source>Start_Self_Tuning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="147"/>
+        <location filename="page200.ui" line="716"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="179"/>
+        <source>Self Tuning Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="182"/>
+        <source>Self Tuning Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="213"/>
+        <location filename="page200.ui" line="219"/>
+        <source>PLC_AnIn_1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="289"/>
+        <source>TEMPERATURE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="311"/>
+        <location filename="page200.ui" line="317"/>
+        <source>KD_SelfTuning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="384"/>
+        <location filename="page200.ui" line="390"/>
+        <source>Cutback_SelfTuning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="457"/>
+        <location filename="page200.ui" line="463"/>
+        <source>KP_SelfTuning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="530"/>
+        <source>K I</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="552"/>
+        <source>K D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="574"/>
+        <source>C. B.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="596"/>
+        <location filename="page200.ui" line="602"/>
+        <source>KI_SelfTuning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page200.ui" line="669"/>
+        <source>K P</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
