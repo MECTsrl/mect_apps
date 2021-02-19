ac35582279411667e0fb03b22753189b6fcf0cd3
Francesco Mandracci <fmandracci@mect.it>
2021-02-19 17:46:36 +0100

    TP1043_01_A/MonitoringViewer: versatile app

---
+++ TP1043_01_A/MonitoringViewer/trendvartable.cpp
@@ -0,0 +1,6 @@
+#include "trendvartable.h"
+
+TrendVarTable::TrendVarTable(QWidget *parent) :
+    QWidget(parent)
+{
+}
---
+++ TP1043_01_A/MonitoringViewer/trendvartable.h
@@ -0,0 +1,18 @@
+#ifndef TRENDVARTABLE_H
+#define TRENDVARTABLE_H
+
+#include <QWidget>
+
+class TrendVarTable : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TrendVarTable(QWidget *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // TRENDVARTABLE_H
