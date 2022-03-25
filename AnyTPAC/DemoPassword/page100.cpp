559850c4ca380dc3681f9815e24e2630302d9434
Andrea Sansoe <asansoe@mect.it>
2022-03-25 12:42:15 +0100

    Fix on Model family (DemoPassword.pro)

--- AnyTPAC/DemoPassword/page100.cpp
+++ AnyTPAC/DemoPassword/page100.cpp
@@ -70,7 +70,7 @@ void page100::reload()
     ui->lblUser->setText(QString("Pwd User:\t[%1] / [%2]") .arg(passwords[pwd_user_e]) .arg(pwd_user_e));
     ui->lblLevel->setText(QString("Level: [%1]") .arg(PasswordsString[active_password]));
     ui->lblMSVersion->setText(QString("MS Version: [%1]") .arg(msVersion, 6, 16, QChar('0')));
-    ui->lblModel->setText(QString("Model: [%1]") .arg(uModel, 6, 16, QChar('0')));
+    ui->lblModel->setText(QString("Model Family: [%1]") .arg(uModel, 6, 16, QChar('0')));
     // Check MS Version
     ui->atcmButtonPassword->setEnabled(msVersion >= minMSVersion);
     ui->atcmButtonSetPwd->setEnabled(msVersion >= minMSVersion);
